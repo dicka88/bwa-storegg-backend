7c14c9716c97ce0ebbf96bb8a5190b409bb0752d
Dicka Ismaji <dickaismaji@gmail.com>
2022-01-15 18:13:31 +0700

    [Feat] add secret key

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT,
   nodeEnv: process.env.NODE_ENV,
-  mongoDbUrl: process.env.MONGODB_URL
+  mongoDbUrl: process.env.MONGODB_URL,
+  secretKey: process.env.SECRET_KEY
 };
