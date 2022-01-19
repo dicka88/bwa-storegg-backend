91ccb7865cb286094b0d65cec69c4cdc6aa12341
Dicka Ismaji <dickaismaji@gmail.com>
2022-01-19 21:29:03 +0700

    [Feat] add bank model

---
+++ app/models/BankModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"]
+  }
+});
+
+module.exports = mongoose.model('bank', Schema);
