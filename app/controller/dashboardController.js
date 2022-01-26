bcccebc865fceecf2f79f1e0e52b81aea5918b36
Dicka Ismaji <dickaismaji@gmail.com>
2022-01-26 16:25:22 +0700

    [Feat ] dashboard page

--- app/controller/dashboardController.js
+++ app/controller/dashboardController.js
@@ -1,6 +1,19 @@
-const index = (req, res) => {
+const PlayerModel = require("../models/PlayerModel");
+const UserModel = require('../models/UserModel');
+const VoucherModel = require('../models/VoucherModel');
+const TransactionModel = require('../models/TransactionModel');
+
+const index = async (req, res) => {
+  const count = {
+    player: await PlayerModel.countDocuments(),
+    user: await UserModel.countDocuments(),
+    transaction: await TransactionModel.countDocuments(),
+    voucher: await VoucherModel.countDocuments()
+  };
+
   res.render('admin/index', {
     title: "Dashboard",
+    count,
     alert: {
       alreadyLogged: req.flash('alreadyLogged')
     }
